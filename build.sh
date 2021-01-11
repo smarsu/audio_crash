de3c2d9380c096d38ce6d0920d60124d68ade112
smarsufan <smarsu196@gmail.com>
2021-01-11 17:26:05 +0800

    copy ios libsmcc.a

--- build.sh
+++ build.sh
@@ -3,39 +3,39 @@ set -x
 
 # ./build-ffmpeg.sh $1
 
-g++ -std=c++11 -O3 audio_convert.cc -o bin/audio_convert -DWITH_MAIN \
-  -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
-  /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a
+# g++ -std=c++11 -O3 audio_convert.cc -o bin/audio_convert -DWITH_MAIN \
+#   -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
+#   /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a
 
-g++ -std=c++11 -O3 audio_volume.cc -o bin/audio_volume -DWITH_MAIN \
-  -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
-  /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a
+# g++ -std=c++11 -O3 audio_volume.cc -o bin/audio_volume -DWITH_MAIN \
+#   -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
+#   /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a
 
-g++ -std=c++11 -O3 media_duration.cc -o bin/media_duration -DWITH_MAIN \
-  -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
-  /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a
+# g++ -std=c++11 -O3 media_duration.cc -o bin/media_duration -DWITH_MAIN \
+#   -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
+#   /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a
 
-g++ -std=c++11 -O3 audio_cut.cc -o bin/audio_cut -DWITH_MAIN \
-  -I /Users/smarsu/tencent/ffmpeg-4.2/x86/include \
-  /Users/smarsu/tencent/ffmpeg-4.2/x86/lib/*.a
+# g++ -std=c++11 -O3 audio_cut.cc -o bin/audio_cut -DWITH_MAIN \
+#   -I /Users/smarsu/tencent/ffmpeg-4.2/x86/include \
+#   /Users/smarsu/tencent/ffmpeg-4.2/x86/lib/*.a
 
-g++ -std=c++11 -O3 bleu.cc -o bin/bleu -DWITH_MAIN \
-  -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
-  /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a
+# g++ -std=c++11 -O3 bleu.cc -o bin/bleu -DWITH_MAIN \
+#   -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
+#   /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a
 
-g++ -std=c++11 -O3 video_thumbnail.cc -o bin/video_thumbnail -DWITH_MAIN \
-  -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
-  /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a
+# g++ -std=c++11 -O3 video_thumbnail.cc -o bin/video_thumbnail -DWITH_MAIN \
+#   -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
+#   /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a
 
-g++ -std=c++11 -O3 edit_pass.cc -o bin/edit_pass -DWITH_MAIN \
-  -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
-  /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a
+# g++ -std=c++11 -O3 edit_pass.cc -o bin/edit_pass -DWITH_MAIN \
+#   -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
+#   /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a
 
-g++ -std=c++11 -O3 audio_gradient.cc -o bin/audio_gradient -DWITH_MAIN \
-  -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
-  /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a \
-  -I /Users/smarsu/tencent/projects/audio_crash/smcc/core \
-  /Users/smarsu/tencent/projects/audio_crash/smcc/build/core/libsmcc_core.a
+# g++ -std=c++11 -O3 audio_gradient.cc -o bin/audio_gradient -DWITH_MAIN \
+#   -I /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/include \
+#   /Users/smarsu/tencent/projects/audio_crash/ffmpeg-4.2/x86/lib/*.a \
+#   -I /Users/smarsu/tencent/projects/audio_crash/smcc/core \
+#   /Users/smarsu/tencent/projects/audio_crash/smcc/build/core/libsmcc_core.a
 
 rm -rf build_ios
 mkdir -p build_ios
@@ -49,6 +49,7 @@ make -j 32
 cd ..
 
 cp build_ios/libaudio_crash.a /Users/smarsu/tencent/projects/flutter_audio_crash/ios/Vendored/
+cp build_ios/smcc/core/libsmcc_core.a /Users/smarsu/tencent/projects/flutter_audio_crash/ios/Vendored/
 # cp build_ios/libaudio_crash.dylib ~/tencent/projects/flutter_audio_crash/ios/Classes/libaudio_crash.dylib
 # cp build_ios/libaudio_crash.dylib /Users/smarsu/tencent/projects/video_editor_flutter/ios/Runner/frameworks/libaudio_crash.dylib
 
